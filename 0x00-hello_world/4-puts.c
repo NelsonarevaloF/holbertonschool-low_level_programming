79c732690bfb89c4bbed4237371c274b9429b81c
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-09-17 04:38:35 +0000

    added the task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * main - this program prints a message
+ * Description: it is printed through the puts function
+ * Return: always return 0
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
