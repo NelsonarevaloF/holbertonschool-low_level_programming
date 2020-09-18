c524ca8353815575e3c602228bd78558f17050b6
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-09-18 04:47:18 +0000

    added from the task 0 until task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - choose if the number is positive or negative
+ * Return: always 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	alphabet = 'A';
+	while (alphabet <= 'Z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar(10);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,9 @@
+# 0x01. C - Variables, if, else, while
+
+in the this project we go to talk about of below topics:
+
+* management the control structures basic
+
+# what do these commands
+    
+You can contact me through of twitter here [![alt text](https://cdn.icon-icons.com/icons2/1254/PNG/128/1495494667-jd13_84467.png)](http://wordpress.com/ "Twitter")
