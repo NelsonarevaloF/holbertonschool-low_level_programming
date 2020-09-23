b90b4266f487044cc7a500a1a49961a3475aba88
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-09-23 00:44:04 +0000

    added task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ * @c: character to evaluate
+ * Return: if the character is lowercase return 1
+ * otherwise return 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= '<')
+		return (1);
+return (0);
+}
