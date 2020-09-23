bd50ad2da04dcf657473d5b2c3a9be1e21add892
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-09-23 03:47:41 +0000

    forgot remove a whitespace

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
 /**
  * _abs - computes the absolute value of an integer
  * @n: number to which we have that find the absolute value
- * Return: the absolute value 
+ * Return: the absolute value
  */
 int _abs(int n)
 {
