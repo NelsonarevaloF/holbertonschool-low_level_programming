3a9c62cdb66a71b37deca13ff08b1d5a29f5db4d
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-10-02 04:54:44 +0000

    added from task 3 and task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,17 +10,17 @@
 void reverse_array(int *a, int n)
 {
 	int cont = 0, i, j;
-        char temporal = 0;
+	char temporal = 0;
 
-        while (a[cont] != '\0')
-        {
-                cont++;
-        }
+	while (a[cont] != '\0')
+	{
+		cont++;
+	}
 
-        for (i = 0, j = n - 1; i < n / 2; i++, j--)
-        {
-                temporal = a[i];
-                a[i] = a[j];
-                a[j] = temporal;
-        }
+	for (i = 0, j = n - 1; i < n / 2; i++, j--)
+	{
+		temporal = a[i];
+		a[i] = a[j];
+		a[j] = temporal;
+	}
 }
