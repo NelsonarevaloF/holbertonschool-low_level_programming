bca02db167d60d2f59bb2761cb9ba4438bccc973
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-10-05 06:25:51 +0000

    added task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *s)
 		j = 0;
 		while (characters[j] != '\0')
 		{
-			if (s[i] == characters[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			if ((s[0] >= 'a' && s[0] <= 'z') || (s[i] == characters[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z')))
 			{
 				s[i + 1] += -32;
 			}
