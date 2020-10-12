6a537d6f5f0b8d961533b3a17d6b3c9f36d2e353
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-10-12 03:44:27 +0000

    forget check through of betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,8 @@
 
 /**
  * main - check the code for Holberton School students.
- *
+ * @argc: size of argv
+ * @argv: arguments from commands line
  * Return: Always 0.
  */
 int main(__attribute__((unused)) int argc, char **argv)
