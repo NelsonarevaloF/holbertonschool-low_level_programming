5f3b61f0bdc8ea7b4caf0443b87e9fb49911a5a1
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-10-14 08:13:00 +0000

    added task 2 fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,40 +9,39 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	int size1 = 0, size2 = 0, i, j;
+	char *cat;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	if (!s1[0] && !s2[0])
 		return (NULL);
 
-	i = 0;
-	j = 0;
+	while (s1[size1] != '\0')
+		size1++;
+	while (s2[size2] != '\0')
+		size2++;
 
-	if (s1)
+	cat = malloc((sizeof(char) * (size1 + size2)) + 1);
+	if (cat == NULL)
+		return (NULL);
+
+	if (size1 != 0)
 	{
-		while (i < len1)
+		if (size2 == 0)
+			size1 += 1;
+		else
+			size2++;
+		for (i = 0; i < size1; i++)
+			cat[i] = s1[i];
+	}
+	if (size2 != 0)
+	{
+		if (size2 == 0)
+			size2 += 1;
+		for (j = 0; j < size2; j++)
 		{
-			s3[i] = s1[i];
-			i++;
+			cat[size1] = s2[j];
+			size1++;
 		}
 	}
-
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
-
-	return (s3);
+	return (cat);
 }
