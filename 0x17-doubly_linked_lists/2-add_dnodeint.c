19432235e267e5a1b3ea984d3495a3dcc3a472e9
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-12-14 04:45:41 +0000

    fixed: i try fixed the error in the checker the task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,11 +22,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		dlistint_t *new = malloc(sizeof(dlistint_t));
 
-		new->n = n;
-		new->prev = NULL;
-		new->next = *head;
-		(*head)->prev = new;
-		*head = new;
+		if (new != NULL)
+		{
+			new->n = n;
+			new->prev = NULL;
+			new->next = *head;
+			(*head)->prev = new;
+			*head = new;
+		}
 	}
 
 return (*head);
