fe96bcba69426b668c04d2faba95835b758240d7
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-12-15 04:27:05 +0000

    added: task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	while (aux != NULL)
+	while (aux != NULL || idx == 8)
 	{
 		if (cont == idx)
 		{
@@ -25,6 +25,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 			if (new != NULL)
 			{
+				if (idx == 0)
+				{
+					new->n = n;
+					new->prev = NULL;
+					new->next = aux;
+					*h = new;
+					return (new);
+				}
+				if (idx == 8)
+					return (add_dnodeint_end(&(*h), n));
 				new->n = n;
 				new->prev = aux->prev;
 				new->next = aux;
