b958ad385b488ace619bec6f668a6fb7b5958f67
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-01-18 05:00:28 +0000

    fixed: I needed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,7 +6,6 @@
  * @value: value associated with the key
  * Return: 1 if success or 0 if fail
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
@@ -14,20 +13,73 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if ((ht && key && value) && *key != '\0')
 	{
-		index = hash_djb2(key) % ht->size;
+		index = key_index((const unsigned char *)key, ht->size);
 		if (ht->array[index] && ht->array[index]->key == ((char *)key))
 		{
-			ht->array[index]->value = strdup((char *)value);
+			ht->array[index]->value = _strdup((char *)value);
 			return (1);
 		}
 		new_node = malloc(sizeof(hash_node_t));
 		if (!new_node)
 			return (0);
 		new_node->key = (char *)key;
-		new_node->value = strdup((char *)value);
+		new_node->value = _strdup((char *)value);
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 		return (1);
 	}
 	return (0);
+}
+/**
+ * _strdup - duplicates a string
+ * @src: string to duplicate
+ * Return: pointer to a string
+ */
+char *_strdup(char *src)
+{
+		char *dst = malloc(_strlen(src) + 1);
+
+		if (dst == NULL)
+			return (NULL);
+		_strcpy(dst, src);
+		return (dst);
+}
+/**
+ * _strlen - return the length the a string
+ * @s: is the pointer the variable "n"
+ * Return: Always 1 (Success)
+ */
+int _strlen(char *s)
+{
+		int i;
+
+		for (i = 0; s[i] != 0; i++)
+		{}
+		return (i);
+}
+/**
+ * _strcpy - function that copies the string pointed to by
+ * src
+ * @dest: char*
+ * @src: char *
+ * Return: nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+		char a = 0;
+		char b = 0;
+
+		if (dest && src)
+		{
+			while (*(src + a))
+				++a;
+			if (src + a <= dest)
+			{
+				do {
+					*(dest + b++) = *src++;
+				} while (a--);
+			}
+			return (dest);
+		}
+return (dest);
 }
