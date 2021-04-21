c38dc304c064ee59aee05e9c6fd843d723eca15a
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-04-21 21:00:25 +0000

    added: task 0, Readme and header file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ * @array:  is a pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (array && i < size)
+	{
+		printf("Value checked array[%i] = [%i]", (int)i, *array);
+		if (*array == value)
+		{
+			return (i);
+		}
+		array++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,12 @@
+# 0x1E-search_algorithms
+
+in this project we go to talk about of below topics:
+
+* What is a search algorithm
+* What is a linear search
+* What is a binary search
+* What is the best search algorithm to use depending on your needs
+
+# what do these commands
+    
+You can contact me through of twitter here [![alt text](https://cdn.icon-icons.com/icons2/1254/PNG/128/1495494667-jd13_84467.png)](https://twitter.com/Near_Fuentes")
